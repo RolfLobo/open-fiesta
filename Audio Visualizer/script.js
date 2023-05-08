c1c9e0bb510e406e47ef662c03304430d976f067
NiladriHazra <NiladriHazra@users.noreply.github.com>
2023-05-08 00:00:00 +0000

    general maintenance and updates

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -118,4 +118,30 @@ const canvasCtx = visualizerCanvas.getContext('2d');
 let audioCtx;
 let analyser;
 let source;
-let isPlaying = false;
+let isPlaying = false;
+
+// Function to set up the Web Audio API
+function setupAudioContext() {
+    if (!audioCtx) {
+        audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+        analyser = audioCtx.createAnalyser();
+        source = audioCtx.createMediaElementSource(audioSource);
+        
+        // Connect the nodes in the audio graph
+        source.connect(analyser);
+        analyser.connect(audioCtx.destination);
+        
+        // Set up the analyser for frequency data
+        analyser.fftSize = 256;
+        const bufferLength = analyser.frequencyBinCount;
+        const dataArray = new Uint8Array(bufferLength);
+        
+        // Start the visualization loop
+        drawVisualizer(bufferLength, dataArray);
+    }
+}
+
+// Function to handle the audio file upload
+audioUpload.addEventListener('change', (e) => {
+    const file = e.target.files[0];
+    if (file) {
