44e7f7749c9ead932a22f8321df118952e1dd7e0
NiladriHazra <NiladriHazra@users.noreply.github.com>
2023-05-10 00:00:00 +0000

    docs: update comments and documentation

--- __tests__/offline/basic.test.ts
+++ __tests__/offline/basic.test.ts
@@ -46,3 +46,28 @@ describe('Basic Offline Functionality', () => {
 import { offlineManager } from '@/lib/offline/manager';
 
 describe('Basic Offline Functionality', () => {
+  beforeEach(() => {
+    // Mock navigator.onLine
+    Object.defineProperty(navigator, 'onLine', {
+      writable: true,
+      value: true,
+    });
+  });
+
+  it('should detect online status', () => {
+    expect(offlineManager.isOnline()).toBe(true);
+  });
+
+  it('should detect offline status', () => {
+    (navigator as any).onLine = false;
+    expect(offlineManager.isOnline()).toBe(false);
+  });
+
+  it('should return status without throwing', async () => {
+    const status = await offlineManager.getStatus();
+    
+    expect(status).toHaveProperty('isOnline');
+    expect(status).toHaveProperty('queuedActionsCount');
+    expect(status).toHaveProperty('syncInProgress');
+    expect(status).toHaveProperty('hasConflicts');
+  });
