0a67725f1cbaf3400988e4228bb3f6ece062fa93
NiladriHazra <NiladriHazra@users.noreply.github.com>
2023-07-08 00:00:00 +0000

    improve error handling for better stability

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1131,3 +1131,28 @@ export default function OpenFiestaChat() {
               radial-gradient(ellipse 85% 65% at 8% 8%, rgba(175, 109, 255, 0.42), transparent 60%),
               radial-gradient(ellipse 75% 60% at 75% 35%, rgba(255, 235, 170, 0.55), transparent 62%),
               radial-gradient(ellipse 70% 60% at 15% 80%, rgba(255, 100, 180, 0.40), transparent 62%),
+              radial-gradient(ellipse 70% 60% at 92% 92%, rgba(120, 190, 255, 0.45), transparent 62%),
+              linear-gradient(180deg, #f7eaff 0%, #fde2ea 100%)
+            `,
+          }}
+        />
+      )}
+
+      {/* Soft vignette for dark mode */}
+      {isDark && (
+        <div
+          className="absolute inset-0 z-0 pointer-events-none"
+          style={{
+            backgroundImage: "radial-gradient(circle at 50% 50%, rgba(0,0,0,0) 55%, rgba(0,0,0,0.5) 100%)",
+            opacity: 0.95,
+          }}
+        />
+      )}
+
+      {/* LaunchScreen splash overlay (same as compare page) */}
+      {showSplash && (
+        <div className="fixed inset-0 z-[9999]">
+          <LaunchScreen backgroundClass={BACKGROUND_STYLES[theme.background].className} dismissed={isHydrated} />
+        </div>
+      )}
+
