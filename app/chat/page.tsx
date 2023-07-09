163a470ace9f8d52409172fbb6fb91c20c9aaa28
NiladriHazra <NiladriHazra@users.noreply.github.com>
2023-07-09 00:00:00 +0000

    docs: update comments and documentation

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1391,3 +1391,15 @@ export default function OpenFiestaChat() {
       <ToastContainer
         position="bottom-right"
         autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </div>
+  )
+}
