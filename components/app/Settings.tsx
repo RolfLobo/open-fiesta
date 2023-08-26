24a530ec682c60a2797cc3a1f422e0c4c84299d4
NiladriHazra <NiladriHazra@users.noreply.github.com>
2023-08-26 00:00:00 +0000

    improve project folder organization

--- components/app/Settings.tsx
+++ components/app/Settings.tsx
@@ -547,3 +547,29 @@ export default function Settings({ compact }: SettingsProps) {
                   <div className="flex items-center justify-between mb-1.5">
                     <label className="block text-sm md:text-base font-medium">
                       Ollama Base URL
+                    </label>
+                    <a
+                      href="https://ollama.com"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className={cn(
+                        "inline-flex items-center gap-1 text-xs px-2.5 py-1 rounded-md border",
+                        isDark
+                          ? "bg-white/10 hover:bg-white/15 border-white/15"
+                          : "bg-black/10 hover:bg-black/15 border-black/15"
+                      )}
+                    >
+                      <ExternalLink size={12} /> Get Ollama
+                    </a>
+                  </div>
+                  <input
+                    value={ollama}
+                    onChange={(e) => setOllama(e.target.value)}
+                    placeholder="http://localhost:11434"
+                    className={cn(
+                      "w-full border rounded-md px-3 py-2.5 text-sm font-mono tracking-wide focus:outline-none focus:ring-2",
+                      isDark
+                        ? "bg-black/40 border-white/15 placeholder:text-zinc-500 focus:ring-white/20"
+                        : "bg-white/40 border-black/15 placeholder:text-gray-500 focus:ring-black/20"
+                    )}
+                  />
