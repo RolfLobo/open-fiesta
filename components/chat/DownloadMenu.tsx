935944e26b73ea1ca68dc10b3b9bc598efcd3169
NiladriHazra <NiladriHazra@users.noreply.github.com>
2023-09-06 00:00:00 +0000

    refactor: improve naming conventions

--- components/chat/DownloadMenu.tsx
+++ components/chat/DownloadMenu.tsx
@@ -164,3 +164,29 @@ export default function DownloadMenu({ thread, selectedModels }: Props) {
             }}
           >
             <div className="p-2">
+              <div className="flex items-center justify-between px-2 py-1 mb-1">
+                <span className="text-xs font-medium text-zinc-300">Export Chat</span>
+                <button
+                  onClick={(e) => {
+                    stopNative(e);
+                    setIsOpen(false);
+                  }}
+                  className="h-5 w-5 inline-flex items-center justify-center rounded hover:bg-white/10 text-zinc-400 hover:text-zinc-200"
+                >
+                  <X size={12} />
+                </button>
+              </div>
+
+              <div className="space-y-1">
+                <button
+                  onClick={handleDownloadMarkdown}
+                  className="w-full flex items-center gap-3 px-3 py-2 text-sm text-left rounded-md hover:bg-white/10 text-zinc-200 hover:text-white transition-colors"
+                >
+                  <FileText size={16} />
+                  <div>
+                    <div className="font-medium">Markdown (.md)</div>
+                    <div className="text-xs text-zinc-400">Plain text format</div>
+                  </div>
+                </button>
+
+                <button
