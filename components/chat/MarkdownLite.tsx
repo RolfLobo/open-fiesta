71103d48b0c0a94cd3fffea1c473dffdd0b69b29
NiladriHazra <NiladriHazra@users.noreply.github.com>
2023-09-13 00:00:00 +0000

    remove redundant code for optimization

--- components/chat/MarkdownLite.tsx
+++ components/chat/MarkdownLite.tsx
@@ -2476,3 +2476,29 @@ function ImageWithSkeleton({ src, alt, filename, isDark }: { src: string; alt: s
                 width="18"
                 height="18"
                 viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1="18" y1="6" x2="6" y2="18" />
+                <line x1="6" y1="6" x2="18" y2="18" />
+              </svg>
+            </button>
+
+            <div
+              className="relative max-w-[95vw] max-h-[95vh]"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <img
+                src={src}
+                alt={alt}
+                className="max-w-[95vw] max-h-[95vh] w-auto h-auto rounded-lg shadow-2xl"
+              />
+            </div>
+          </div>,
+          document.body,
+        )}
+    </>
+  );
