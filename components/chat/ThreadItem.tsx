430d5ee78d3995ba2231398982bc94bbcb2c61de
NiladriHazra <NiladriHazra@users.noreply.github.com>
2023-09-22 00:00:00 +0000

    update dependencies and config files

--- components/chat/ThreadItem.tsx
+++ components/chat/ThreadItem.tsx
@@ -123,3 +123,29 @@ export default function ThreadItem({
       className={`w-full px-3 py-1.5 xl:py-2.5 rounded-lg text-sm border flex items-center justify-between gap-2 group relative transition-all duration-200 ${
         isActive
           ? 'bg-white/15 border-white/25 ring-1 ring-white/30 shadow-sm'
+          : thread.pageType === 'home'
+            ? 'bg-orange-500/10 border-orange-300/20 hover:bg-orange-500/15'
+            : 'bg-white/5 border-white/10 hover:bg-white/10'
+      }`}
+    >
+      <button
+        onClick={onSelect}
+        className="min-w-0 text-left flex-1 truncate"
+        title={thread.title || 'Untitled'}
+      >
+        {thread.title || 'Untitled'}
+      </button>
+      
+      <div className="flex items-center gap-1">
+        {/* Kebab menu trigger */}
+        <button
+          aria-label="Thread actions"
+          title="More"
+          onClick={(e) => {
+            e.stopPropagation();
+            onMenuToggle(thread.id);
+          }}
+          className="h-7 w-7 shrink-0 inline-flex items-center justify-center rounded-md border border-white/10 bg-white/5 hover:bg-white/10 text-zinc-300"
+        >
+          <MoreVertical size={14} />
+        </button>
