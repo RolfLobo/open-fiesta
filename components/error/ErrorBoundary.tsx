7c8ad8caebe75a4ee8c472583fbf0bb913d1d9e1
NiladriHazra <NiladriHazra@users.noreply.github.com>
2023-10-01 00:00:00 +0000

    fix: small bug fixes and adjustments

--- components/error/ErrorBoundary.tsx
+++ components/error/ErrorBoundary.tsx
@@ -80,3 +80,29 @@ import React, { Component, ErrorInfo, ReactNode } from 'react';
 
 interface Props {
   children: ReactNode;
+  fallback?: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+  error?: Error;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error): State {
+    return { hasError: true, error };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error('Error caught by Error Boundary:', error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        this.props.fallback || (
