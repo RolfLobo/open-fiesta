c8ef55cfd35a7b370bcad2a8d26860417eb7fae2
NiladriHazra <NiladriHazra@users.noreply.github.com>
2023-10-03 00:00:00 +0000

    update project structure for better maintainability

--- components/error/RetroNotFound.tsx
+++ components/error/RetroNotFound.tsx
@@ -638,3 +638,29 @@ const RetroNotFound: React.FC = () => {
             <div className="a2d" />
             <div className="a_base" />
           </div>
+          <div className="tv">
+            <div className="cruve">
+              <svg
+                className="curve_svg"
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                viewBox="0 0 189.929 189.929"
+                xmlSpace="preserve"
+              >
+                <path d="M70.343,70.343c-30.554,30.553-44.806,72.7-39.102,115.635l-29.738,3.951C-5.442,137.659,11.917,86.34,49.129,49.13 C86.34,11.918,137.664-5.445,189.928,1.502l-3.95,29.738C143.041,25.54,100.895,39.789,70.343,70.343z" />
+              </svg>
+            </div>
+            <div className="display_div">
+              <div className="screen_out">
+                <div className="screen_out1">
+                  <div className="screen">
+                    <span className="notfound_text"> NOT FOUND</span>
+                  </div>
+                  <div className="screenM">
+                    <span className="notfound_text"> NOT FOUND</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="lines">
