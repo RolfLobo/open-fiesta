5888a5ec79ff26d1d9211d0c342967862d9a9922
NiladriHazra <NiladriHazra@users.noreply.github.com>
2023-10-16 00:00:00 +0000

    small tweaks and code enhancements

--- components/modals/ExpandedChatModal.tsx
+++ components/modals/ExpandedChatModal.tsx
@@ -230,3 +230,29 @@ export default function ExpandedChatModal({
       document.body.style.overflow = 'unset';
     };
   }, [isOpen]);
+
+  useEffect(() => {
+    const handleEscape = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('keydown', handleEscape);
+    }
+
+    return () => {
+      document.removeEventListener('keydown', handleEscape);
+    };
+  }, [isOpen, onClose]);
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center">
+      {/* Backdrop */}
+      <div
+        className={cn(
+          "absolute inset-0 backdrop-blur-sm",
+          isDark ? "bg-black/80" : "bg-white/80"
