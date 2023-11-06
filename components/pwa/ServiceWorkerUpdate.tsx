757a01ab38367ce28d8a6f5cdfe271bd6728f12d
NiladriHazra <NiladriHazra@users.noreply.github.com>
2023-11-06 00:00:00 +0000

    small tweaks and code enhancements

--- components/pwa/ServiceWorkerUpdate.tsx
+++ components/pwa/ServiceWorkerUpdate.tsx
@@ -176,3 +176,29 @@ export const ServiceWorkerUpdate: React.FC<ServiceWorkerUpdateProps> = ({
               onClick={handleUpdate}
               disabled={isUpdating}
               className="bg-white text-blue-600 px-3 py-1 rounded text-xs font-medium hover:bg-gray-100 disabled:opacity-50 disabled:cursor-not-allowed"
+            >
+              {isUpdating ? 'Updating...' : 'Update'}
+            </button>
+            <button
+              onClick={handleDismiss}
+              className="text-white/80 hover:text-white px-3 py-1 rounded text-xs"
+            >
+              Later
+            </button>
+          </div>
+        </div>
+        <button
+          onClick={handleDismiss}
+          className="text-white/60 hover:text-white ml-2 p-1"
+          aria-label="Close"
+        >
+          <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
+            <path
+              fillRule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
