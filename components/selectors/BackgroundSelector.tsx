fc4e9c99fb3640972988146cd255f68b700b3406
NiladriHazra <NiladriHazra@users.noreply.github.com>
2023-11-07 00:00:00 +0000

    fix: adjust function parameters for clarity

--- components/selectors/BackgroundSelector.tsx
+++ components/selectors/BackgroundSelector.tsx
@@ -75,3 +75,29 @@ export default function BackgroundSelector({
   className = '',
   showLabels = true,
   previewSize = 'md',
+}: BackgroundSelectorProps) {
+  const { theme, setBackground } = useTheme();
+
+  const sizeClasses = {
+    sm: 'h-12',
+    md: 'h-16',
+    lg: 'h-20',
+  };
+
+  const handleBackgroundChange = (background: BackgroundStyle) => {
+    setBackground(background);
+  };
+
+  return (
+    <div className={`space-y-3 ${className}`}>
+      {showLabels && <span className="text-sm font-medium text-white/80">Background Style:</span>}
+      <div className="grid grid-cols-2 gap-2">
+        {Object.values(BACKGROUND_STYLES).map((bg) => (
+          <button
+            key={bg.id}
+            onClick={() => handleBackgroundChange(bg.id)}
+            className={`p-3 rounded-lg border transition-colors text-left ${
+              theme.background === bg.id
+                ? 'border-white/30 bg-white/10'
+                : 'border-white/10 bg-white/5 hover:bg-white/8'
+            }`}
