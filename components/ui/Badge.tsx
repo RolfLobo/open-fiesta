08ca5dd6c91f09fbaaca315546f2968c7b0d5452
NiladriHazra <NiladriHazra@users.noreply.github.com>
2023-11-22 00:00:00 +0000

    improve project folder organization

--- components/ui/Badge.tsx
+++ components/ui/Badge.tsx
@@ -65,3 +65,29 @@ const badgeVariants = cva(
         outline:
           "text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground",
       },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+)
+
+function Badge({
+  className,
+  variant,
+  asChild = false,
+  ...props
+}: React.ComponentProps<"span"> &
+  VariantProps<typeof badgeVariants> & { asChild?: boolean }) {
+  const Comp = asChild ? Slot : "span"
+
+  return (
+    <Comp
+      data-slot="badge"
+      className={cn(badgeVariants({ variant }), className)}
+      {...props}
+    />
+  )
+}
+
+export { Badge, badgeVariants }
