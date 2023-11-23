f4bc2cf2060129004f313787d8982412183983e6
NiladriHazra <NiladriHazra@users.noreply.github.com>
2023-11-23 00:00:00 +0000

    optimize code for performance

--- components/ui/CopyToClipboard.tsx
+++ components/ui/CopyToClipboard.tsx
@@ -58,3 +58,29 @@ export const CopyToClipboard = ({
 import { cn, copyToClipboard } from '@/lib/utils';
 import { CheckIcon, CopyIcon, XIcon } from 'lucide-react';
 import { useOptimistic, useTransition } from 'react';
+
+export const CopyToClipboard = ({
+  getText,
+  iconSize = 12,
+  className,
+  timeout = 1200,
+  ...props
+}: {
+  getText: () => string;
+  timeout?: number;
+  iconSize?: number;
+} & React.ComponentProps<'button'>) => {
+  const [state, setState] = useOptimistic<'idle' | 'copied' | 'failed'>('idle');
+  const [, startTransition] = useTransition();
+
+  return (
+    <button
+      onClick={() => {
+        startTransition(async () => {
+          try {
+            await copyToClipboard(getText());
+            setState('copied');
+          } catch (error) {
+            console.error('Error copying to clipboard', error);
+            setState('failed');
+          }
