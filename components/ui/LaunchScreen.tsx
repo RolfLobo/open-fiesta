eabec6044b5812581b0a2f4210ab51f96ae669b2
NiladriHazra <NiladriHazra@users.noreply.github.com>
2023-11-23 00:00:00 +0000

    improve error handling for better stability

--- components/ui/LaunchScreen.tsx
+++ components/ui/LaunchScreen.tsx
@@ -179,3 +179,29 @@ interface LaunchScreenProps {
 }
 
 /**
+ * A themable, accessible launch screen UI with optional branding and PWA standalone adjustments.
+ *
+ * Renders a full-screen centered status card with optional logo, title, subtitle, and a subtle
+ * progress sheen. Adapts colors for dark/light themes, supports hiding via `dismissed`, and when
+ * running as a PWA (or when `isPWA` is true) applies safe-area padding and a `pwa-launch-screen`
+ * class for standalone display.
+ *
+ * @param backgroundClass - Optional additional CSS classes applied to the outer container.
+ * @param title - Main heading text shown on the card (defaults to "Open Fiesta").
+ * @param subtitle - Subheading text shown below the title (defaults to "Warming things up…").
+ * @param logoSrc - URL for the brand/logo image; when falsy the logo block is omitted.
+ * @param dismissed - When true, reduces opacity and disables pointer events to hide the screen.
+ * @param isPWA - If provided, forces PWA standalone mode; otherwise standalone detection is used.
+ * @returns A React element representing the launch screen.
+ */
+export default function LaunchScreen({
+  backgroundClass = '',
+  title = 'Open Fiesta',
+  subtitle = 'Warming things up…',
+  logoSrc = '/brand.svg',
+  dismissed = false,
+  isPWA,
+}: LaunchScreenProps) {
+  const { theme } = useTheme();
+  const isDark = theme.mode === 'dark';
+  const isStandaloneMode = isPWA ?? isStandalone();
