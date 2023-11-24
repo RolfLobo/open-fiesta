6ec590f2dfee7bab5f09c1fc1ea200f6d39dead2
NiladriHazra <NiladriHazra@users.noreply.github.com>
2023-11-24 00:00:00 +0000

    refactor: minor code structure improvements

--- components/ui/ai-input.tsx
+++ components/ui/ai-input.tsx
@@ -285,3 +285,30 @@ function useAutoResizeTextarea({ minHeight, maxHeight }: UseAutoResizeTextareaPr
     (reset?: boolean) => {
       const textarea = textareaRef.current;
       if (!textarea) return;
+
+      if (reset) {
+        textarea.style.height = `${minHeight}px`;
+        return;
+      }
+
+      textarea.style.height = `${minHeight}px`;
+      const newHeight = Math.max(
+        minHeight,
+        Math.min(textarea.scrollHeight, maxHeight ?? Number.POSITIVE_INFINITY),
+      );
+
+      textarea.style.height = `${newHeight}px`;
+    },
+    [minHeight, maxHeight],
+  );
+
+  useEffect(() => {
+    const textarea = textareaRef.current;
+    if (textarea) {
+      textarea.style.height = `${minHeight}px`;
+    }
+  }, [minHeight]);
+
+  useEffect(() => {
+    const handleResize = () => adjustHeight();
+    window.addEventListener('resize', handleResize);
