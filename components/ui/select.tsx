1f7c0d8763bc967269f3d6ead12ecaaccf4bb4ce
NiladriHazra <NiladriHazra@users.noreply.github.com>
2023-11-28 00:00:00 +0000

    docs: update comments and documentation

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -143,3 +143,11 @@ export {
   SelectScrollUpButton,
   SelectScrollDownButton,
 }
+
+"use client"
+
+import * as React from "react"
+import * as SelectPrimitive from "@radix-ui/react-select"
+import { Check, ChevronDown, ChevronUp } from "lucide-react"
+
+import { cn } from "@/lib/utils"
