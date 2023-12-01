82ba1fb256c744da1a4e14fb4c3f08613096850f
NiladriHazra <NiladriHazra@users.noreply.github.com>
2023-12-01 00:00:00 +0000

    enhance code readability

--- components/ui/text-area.tsx
+++ components/ui/text-area.tsx
@@ -18,3 +18,15 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(({ classNa
 Textarea.displayName = "Textarea"
 
 export { Textarea }
+
+import * as React from "react"
+import { cn } from "@/lib/utils"
+
+export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(({ className, ...props }, ref) => {
+  return (
+    <textarea
+      className={cn(
+        "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
+        className,
