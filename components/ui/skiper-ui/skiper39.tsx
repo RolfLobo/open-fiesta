0c9a8a0b7183f3a315180c17932ff987b133996d
NiladriHazra <NiladriHazra@users.noreply.github.com>
2023-12-01 00:00:00 +0000

    update build files and scripts

--- components/ui/skiper-ui/skiper39.tsx
+++ components/ui/skiper-ui/skiper39.tsx
@@ -435,3 +435,29 @@ const CrowdCanvas = ({ src, rows = 15, cols = 7 }: CrowdCanvasProps) => {
     }: {
       image: HTMLImageElement;
       rect: number[];
+    }): Peep => {
+      const peep: Peep = {
+        image,
+        rect: [],
+        width: 0,
+        height: 0,
+        drawArgs: [],
+        x: 0,
+        y: 0,
+        anchorY: 0,
+        scaleX: 1,
+        walk: null,
+        setRect: (rect: number[]) => {
+          peep.rect = rect;
+          peep.width = rect[2];
+          peep.height = rect[3];
+          peep.drawArgs = [peep.image, ...rect, 0, 0, peep.width, peep.height];
+        },
+        render: (ctx: CanvasRenderingContext2D) => {
+          ctx.save();
+          ctx.translate(peep.x, peep.y);
+          ctx.scale(peep.scaleX, 1);
+          ctx.drawImage(
+            peep.image,
+            peep.rect[0],
+            peep.rect[1],
