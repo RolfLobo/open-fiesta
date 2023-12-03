5806fa3434043090d1d77a42d4a3bd4807f0b9fe
NiladriHazra <NiladriHazra@users.noreply.github.com>
2023-12-03 00:00:00 +0000

    small tweaks and code enhancements

--- jest.setup.js
+++ jest.setup.js
@@ -201,3 +201,29 @@ jest.mock('next/router', () => ({
       beforePopState: jest.fn(),
       events: {
         on: jest.fn(),
+        off: jest.fn(),
+        emit: jest.fn(),
+      },
+    }
+  },
+}))
+
+// Mock next/navigation
+jest.mock('next/navigation', () => ({
+  useRouter() {
+    return {
+      push: jest.fn(),
+      replace: jest.fn(),
+      prefetch: jest.fn(),
+      back: jest.fn(),
+      forward: jest.fn(),
+      refresh: jest.fn(),
+    }
+  },
+  useSearchParams() {
+    return new URLSearchParams()
+  },
+  usePathname() {
+    return '/'
+  },
+}))
