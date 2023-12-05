a8a6bf4cc32332f885803d48c6cbfeaf60c7a5ff
NiladriHazra <NiladriHazra@users.noreply.github.com>
2023-12-05 00:00:00 +0000

    remove redundant code for optimization

--- lib/__tests__/generateThemeClasses.test.ts
+++ lib/__tests__/generateThemeClasses.test.ts
@@ -25,3 +25,23 @@ describe('generateThemeClasses', () => {
     expect(generateThemeClasses(config)).toMatchSnapshot();
   });
 });
+
+import { generateThemeClasses } from '../themes';
+
+describe('generateThemeClasses', () => {
+  it('should generate correct class names for default theme config', () => {
+    const config = {
+      mode: 'light' as const,
+      accent: 'crimson' as const,
+      font: 'geist' as const,
+      background: 'gradient' as const,
+      badgePair: 'red-gold' as const,
+      chatInputStyle: 'default' as const,
+    };
+    expect(generateThemeClasses(config)).toMatchSnapshot();
+  });
+
+  it('should generate correct class names for custom theme config', () => {
+    const config = {
+      mode: 'dark' as const,
+      accent: 'emerald' as const,
