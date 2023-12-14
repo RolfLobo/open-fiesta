2145ba130ddd1e0fd2c05c8d04a0d7b152434def
NiladriHazra <NiladriHazra@users.noreply.github.com>
2023-12-14 00:00:00 +0000

    general maintenance and updates

--- lib/chatActions.ts
+++ lib/chatActions.ts
@@ -1510,3 +1510,29 @@ export function createChatActions({
               // Add placeholder for super fast typing animation
               const placeholderTs = Date.now();
               const placeholder: ChatMessage = {
+                role: 'assistant',
+                content: '',
+                modelId: m.id,
+                ts: placeholderTs,
+              };
+              setThreads((prev) =>
+                prev.map((t) =>
+                  t.id === thread.id
+                    ? { ...t, messages: [...(t.messages ?? nextHistory), placeholder] }
+                    : t,
+                ),
+              );
+              
+              // Super fast typing effect with requestAnimationFrame for smooth scrolling
+              let i = 0;
+              const step = Math.max(2, Math.ceil(content.length / 40)); // Smaller steps for smoother animation
+              let lastUpdate = 0;
+              const animate = (timestamp: number) => {
+                if (timestamp - lastUpdate >= 12) { // Throttle to ~83fps for smoothness
+                  i = Math.min(content.length, i + step);
+                  const chunk = content.slice(0, i);
+                  setThreads((prev) =>
+                    prev.map((t) => {
+                      if (t.id !== thread.id) return t;
+                      const msgs = (t.messages ?? []).map((msg) =>
+                        msg.ts === placeholderTs && msg.modelId === m.id
