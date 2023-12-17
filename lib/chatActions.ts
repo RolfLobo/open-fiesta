2d946509ccf449f256b5cd97eabf85f29193c42e
NiladriHazra <NiladriHazra@users.noreply.github.com>
2023-12-17 00:00:00 +0000

    improve project folder organization

--- lib/chatActions.ts
+++ lib/chatActions.ts
@@ -1822,3 +1822,29 @@ export function createChatActions({
                   modelId: m.id,
                   ts: placeholderTs,
                 };
+                setThreads((prev) =>
+                  prev.map((t) =>
+                    t.id === thread.id
+                      ? { ...t, messages: [...(t.messages ?? nextHistory), placeholder] }
+                      : t,
+                  ),
+                );
+                
+                // Super fast typing effect with requestAnimationFrame for smooth scrolling
+                let i = 0;
+                const step = Math.max(2, Math.ceil(full.length / 40)); // Smaller steps for smoother animation
+                let lastUpdate = 0;
+                const animate = (timestamp: number) => {
+                  if (timestamp - lastUpdate >= 12) { // Throttle to ~83fps for smoothness
+                    i = Math.min(full.length, i + step);
+                    const chunk = full.slice(0, i);
+                    setThreads((prev) =>
+                      prev.map((t) => {
+                        if (t.id !== thread.id) return t;
+                        const msgs = (t.messages ?? []).map((msg) =>
+                          msg.ts === placeholderTs && msg.modelId === m.id
+                            ? { ...msg, content: chunk }
+                            : msg,
+                        );
+                        return { ...t, messages: msgs };
+                      }),
